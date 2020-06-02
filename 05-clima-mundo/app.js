6a1eba57699fe3799533eb419fc755f09eae5e37
Juan Espinoza <juan.espinoza@fluxit.com.ar>
2020-06-02 19:58:27 -0300

    Se realiza una petición a Rapid API

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,1 +1,24 @@
-const argv = require('yargs').options({})
+const axios = require('axios');
+
+const argv = require('yargs').options({
+    direccion: {
+        alias: 'd',
+        desc: 'Dirección de la ciudad para obtener el clima',
+        demand: true
+    }
+}).argv;
+
+const encoderUrl = encodeURI(argv.direccion);
+
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encoderUrl}`,
+    headers: { 'x-rapidapi-key': '045d915725msh161414a49600870p1f044ajsn045e73e3b5ed' }
+});
+
+instance.get()
+    .then(resp => {
+        console.log(resp.data.Results[0]);
+    })
+    .catch(err => {
+        console.log('Error!', err);
+    })
