9af6edfcb873ae8be72995faac21a694d3152297
Juan Espinoza <juan.espinoza@fluxit.com.ar>
2020-06-01 21:20:40 -0300

    Se guardan las tareas en un archivo JSON

--- 04-por-hacer/app.js
+++ 04-por-hacer/app.js
@@ -1,12 +1,14 @@
 const argv = require('./config/yargs').argv;
 
+const porHacer = require('./por-hacer/por-hacer')
 
 console.log(argv);
 
 let comando = argv._[0];
 switch (comando) {
     case 'crear':
-        console.log('Crear por hacer');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
     case 'listar':
         console.log('Mostrar todas las tareas por hacer');
---
+++ 04-por-hacer/por-hacer/por-hacer.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDb = () => {
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se pudo grabar', err)
+    });
+}
+
+const crear = (descripcion) => {
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDb();
+    return porHacer;
+}
+
+module.exports = {
+    crear
+}
